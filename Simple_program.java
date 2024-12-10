930e98d21996d406477cfc3e56468d240ac6b3b2
Gunjan <GUNJAN.Y-BHOYAR@CAPGEMINI.COM>
2024-12-10 13:35:33 +0530

    first commit

---
+++ Simple_program.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class Simple_program {
+    public static void main(String[] args)
+    {
+        System.out.println("I am a Geek");
+        System.out.println("this is java program");
+        System.out.println(new Date());
+    }
+}
